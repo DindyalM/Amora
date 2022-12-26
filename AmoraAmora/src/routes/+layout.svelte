c350f5cf1335fc462aaef45b6e63d6f94fe65df4
DindyalM <32683916+DindyalM@users.noreply.github.com>
2022-12-26 16:13:30 -0500

    adjusted grids for landing page needs api

---
+++ AmoraAmora/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+  import "../app.css";
+</script>
+<header>
+<nav class="w-full bg-red-700 max-h-28">
+  <div class="flex mb-0 h-12">
+    <div class="w-full bg-red-700 max-h-14"></div> 
+  </div>
+
+  <div class="flex mb-2">
+    <div class="w-1/3 bg-gray-400 h-12">
+      <h1>home,men,women</h1>
+    </div>
+    <div class="w-1/3 bg-gray-500 h-12">
+      <h1>AMORA</h1>
+    </div>
+    <div class="w-1/3 bg-gray-400 h-12">help</div>
+  </div>
+
+</nav>
+</header>
+<slot />
--- AmoraAmora/src/routes/+page.svelte
+++ AmoraAmora/src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import "../app.css";
+</script>
+<div class="flex">
+  <div class="w-1/6 bg-slate-100">w-1/3</div>
+
+    <div class="w-4/6">
+      <div class="grid grid-cols-4 gap-1">
+      <div class = "h-96 bg-slate-500">01</div>
+      <div class = "h-96 bg-slate-500">02</div>
+      <div class = "h-96 bg-slate-500">03</div>
+      <div class = "h-96 bg-slate-500">04</div>
+      </div>
+    </div>
+
+  <div class="w-1/6 bg-slate-100">w-1/3</div>
+
+</div>
+
+
+
+
+
+
