c9bf2e6b80e3df43b7a9447d82715936a28c6bce
DindyalM <32683916+DindyalM@users.noreply.github.com>
2022-12-26 20:41:40 -0500

    1 file modified

--- AmoraAmora/src/routes/+page.svelte
+++ AmoraAmora/src/routes/+page.svelte
@@ -2,28 +2,33 @@
   import "../app.css";
 </script>
 <div class="flex">
-  <div class="w-1/6 bg-slate-100">w-1/3</div>
+  <div class="w-1/6 bg-slate-100"></div>
 
-    <div class="w-4/6">
+  <div class="w-4/6">
       <div class="grid grid-cols-4 gap-1">
-      <div class = "h-96 bg-slate-500">
-        <img class='h-full' src="https://cdn.shopify.com/s/files/1/0293/9277/products/12-20-22Studio1_TH_A_12-04-18_30_D8616_Black_22895_JB_468x@2x.jpg?v=1671728260" alt="">
-      </div>
-      <div class = "h-96 bg-slate-500">
-        <img class= 'h-full'src="https://img.ltwebstatic.com/images3_pi/2022/10/28/1666942946c3f77c2bb33836d5b23f2717e096eb80_thumbnail_900x.webp" alt="">
-      </div>
-      <div class = "h-96 bg-slate-500">
-        <img class= 'h-full' src="https://cdn.shopify.com/s/files/1/0528/2961/8334/products/Understance-Ines-Bra-PO-Navy-Front_900x.jpg?v=1652833917" alt="">
-      </div>
-      <div class = "h-96 bg-slate-500">
-      <img class= "h-full" src="https://cdn.shopify.com/s/files/1/1981/3517/products/076131816257-21-1_64001426-4037-470f-a1ca-46dfb1261a5d_480x720_crop_center.jpg?v=1662647544" alt="">
-      </div>
-      </div>
-    </div>
+        
+        <div class = "h-96 bg-slate-500">
+          <img class='h-full' src="https://cdn.shopify.com/s/files/1/0293/9277/products/12-20-22Studio1_TH_A_12-04-18_30_D8616_Black_22895_JB_468x@2x.jpg?v=1671728260" alt="">
+        </div>
 
-  <div class="w-1/6 bg-slate-100">w-1/3</div>
+        <div class = "h-96 bg-slate-500">
+          <img class= 'h-full'src="https://img.ltwebstatic.com/images3_pi/2022/10/28/1666942946c3f77c2bb33836d5b23f2717e096eb80_thumbnail_900x.webp" alt="">
+        </div>
 
+        <div class = "h-96 bg-slate-500">
+          <img class= 'h-full' src="https://cdn.shopify.com/s/files/1/0528/2961/8334/products/Understance-Ines-Bra-PO-Navy-Front_900x.jpg?v=1652833917" alt="">
+        </div>
+
+        <div class = "h-96 bg-slate-500">
+          <img class= "h-full" src="https://cdn.shopify.com/s/files/1/1981/3517/products/076131816257-21-1_64001426-4037-470f-a1ca-46dfb1261a5d_480x720_crop_center.jpg?v=1662647544" alt="">
+        </div>
+   </div>
+
+  
+  </div>
+  
 </div>
+
 
 
 
