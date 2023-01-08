b854aef689c5f2567549fc0c9d66a9cf644cdbb3
DindyalM <32683916+DindyalM@users.noreply.github.com>
2023-01-08 18:15:36 -0500

    3 files modified changed for Joan

--- AmoraAmora/src/routes/+page.svelte
+++ AmoraAmora/src/routes/+page.svelte
@@ -3,30 +3,25 @@
 </script>
 <div class="flex">
   <div class="w-1/6 bg-slate-100"></div>
-
-  <div class="w-4/6">
-      <div class="grid grid-cols-4 gap-1">
-        
-        <div class = "h-96 bg-slate-500">
-          <img class='h-full' src="https://cdn.shopify.com/s/files/1/0293/9277/products/12-20-22Studio1_TH_A_12-04-18_30_D8616_Black_22895_JB_468x@2x.jpg?v=1671728260" alt="">
-        </div>
-
-        <div class = "h-96 bg-slate-500">
-          <img class= 'h-full'src="https://img.ltwebstatic.com/images3_pi/2022/10/28/1666942946c3f77c2bb33836d5b23f2717e096eb80_thumbnail_900x.webp" alt="">
-        </div>
-
-        <div class = "h-96 bg-slate-500">
-          <img class= 'h-full' src="https://cdn.shopify.com/s/files/1/0528/2961/8334/products/Understance-Ines-Bra-PO-Navy-Front_900x.jpg?v=1652833917" alt="">
-        </div>
-
-        <div class = "h-96 bg-slate-500">
-          <img class= "h-full" src="https://cdn.shopify.com/s/files/1/1981/3517/products/076131816257-21-1_64001426-4037-470f-a1ca-46dfb1261a5d_480x720_crop_center.jpg?v=1662647544" alt="">
-        </div>
-   </div>
-
-  
+  <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+  <div class="md:flex">
+    <div class="md:shrink-0">
+      <img class="h-48 w-full object-cover md:h-full md:w-48" src="/img/building.jpg" alt="Modern building architecture">
+    </div>
+    <div class="p-8">
+      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
+      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accomodation for your team</a>
+      <div name="p-8">
+        <p class="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
+    </div>
+      
+    </div>
   </div>
-  
+</div>
+
+<div></div>
+
+ 
 </div>
 
 
